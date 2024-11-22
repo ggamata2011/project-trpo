b68d01fbd8e35b67e46b4e7ee55290e10fe2d5f0
justinsanchez12 <justinlsanchez12@gmail.com>
2024-11-21 18:37:18 -0800

    Added transactions object so user can see latest 3 transactions on the console

--- stripe-connector/app.js
+++ stripe-connector/app.js
@@ -14,14 +14,26 @@ const bodyParse = require('body-parser');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 //List out customers on the console
-(async () => {
-    const customers = await stripe.customers.list();
+/*(async () => {
+    const customers = await stripe.customers.list({limit: 3});
     console.log('Hello, Authentication');
     console.log(customers); //Print out customers
 })()
+*/
+
+//list out last 10 transactions on the console
+ (async () => {
+    try {
+        const charges = await stripe.charges.list({limit:3})
+        console.log('Here are the recent BamRec Transactions!');
+        console.log(charges);
+    } catch (error) {
+        console.error("Error fetching charges: ", error);
+    }
+})(); 
 
 // Starting server
-app.listen(3002, function () {
+app.listen(3001, function () {
     console.log('Server running on port 3001');
     console.log('https://localhost:3001');
 });
